e961ac120a13815d27e9670e2d72537935727ff8
csf <shuangfeng1993@163.com>
2021-01-08 00:38:52 +0800

    exit port and host

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,7 @@
 
 module.exports = {
   router: {
+    base: '/nuxt-realworld-classdemo',
     // 自定义高亮类名，默认nuxt-link-active
     linkActiveClass: 'active',
     // 自定义路由表规则
@@ -60,7 +61,7 @@ module.exports = {
    // host默认是localhost，这个只提供本机服务，如果要提供外部访问，这里要设置成0.0.0.0，他就会监听所有的网卡地址。如果是本地就是整个局域网都可以访问到。
    server: {
     host: '0.0.0.0',
-    port: 3000
+    port: 80
    },
 
    // 注册插件,前面要加~，从根路径出发的意思
